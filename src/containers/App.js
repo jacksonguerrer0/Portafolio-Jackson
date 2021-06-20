629b7381b1e95717fe2037db71eb77b7272ece95
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-06-20 17:32:24 -0500

    Empaquetando

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,11 +3,11 @@ import '../styles/index.css';
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Main from '../components/Main';
-import Presentacion from '../components/Presentacion';
-import Proyecto from '../components/Proyecto';
-import Certificado from '../components/Certificado';
-import Contacto from '../components/Contacto';
-import Footer from '../components/Footer';
+import Presentacion from './components/Presentacion';
+import Proyecto from './components/Proyecto';
+import Certificado from './components/Certificado';
+import Contacto from './components/Contacto';
+import Footer from './components/Footer';
 
 
 const App = ()=> (
